f1edd7438be5f7a7badb531b998e89bb8e494331
ruchikxxii <ruchik710@gmail.com>
2022-09-17 16:44:07 +0530

    added test dropdown

--- src/components/FrontPage/NavbarDesktop.js
+++ src/components/FrontPage/NavbarDesktop.js
@@ -1,11 +1,29 @@
 import classes from './NavbarDesktop.module.css'
+import { useState } from 'react';
 
 const NavbarDesktop = () => {
+
+    const [drop,setDrop] = useState(false)
+
+    const TestDropdown = () => {
+        return (<div className={classes.dropdown}>
+            <h2>Hello World</h2>
+            <h2>World</h2>
+        </div>);
+    }
+
+    const changeDrop = () => {
+        setDrop(state => {return !state})
+    }
+
     return (
         <div className={classes.navbar}>
             <h1 className={classes['nav-logo']}>LOGO</h1>
             <div className={classes['nav-items']}>
-                <h2 className={classes['nav-item']}>Home</h2> 
+                <div onMouseEnter={changeDrop} onMouseLeave={changeDrop} className={classes['nav-item']}>
+                    <h2>Home</h2>
+                    {drop && <TestDropdown />}
+                </div>
                 <h2 className={classes['nav-item']}>About Us</h2>
                 <h2 className={classes['nav-item']}>Our Work</h2>
                 <h2 className={classes['nav-item']}>Work With Us</h2>
