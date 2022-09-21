7496c71416af745ca9bd615d309985846b411f85
ruchikxxii <ruchik710@gmail.com>
2022-09-21 14:22:31 +0530

    added responsive welcome page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import AboutUs from './components/AboutUs/AboutUs';
 import FrontPage from './components/FrontPage/FrontPage';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
     <React.Fragment>
       <FrontPage />
       <AboutUs />
+      <Welcome />
     </React.Fragment>
   );
 }
