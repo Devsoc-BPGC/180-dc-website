8162056d3cceb2e6c6f868cf0cd4bc6e1df1b358
ruchikxxii <ruchik710@gmail.com>
2022-09-23 14:36:32 +0530

    ui imporvements for clients section

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -4,11 +4,13 @@ import Flip from '../UI/Flip';
 const Clients = () => {
     return (
         <div className={classes['clients']}>
+            <div className={classes.line}></div>
             <div className={classes['client-title']}>
-                <h1>For Clients</h1>
-                <p>We Greatly value our clients. Hear what they have to say for us.</p>
+                <p>FOR CLIENTS</p>
+                <h1>We greatly value our clients.</h1>
+                <h1>Hear what they have to say for us.</h1>
             </div>
-            <div className={classes['card']}>
+            <div className={classes['cards']}>
                 <div className={classes['card-row']}>
                     <Flip />
                     <Flip />
