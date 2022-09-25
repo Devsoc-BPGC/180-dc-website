ef723477f041ebcd3ecf6ab8181b5c11cc9330b7
ruchikxxii <ruchik710@gmail.com>
2022-09-25 14:51:13 +0530

    added values page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Capabilities from './components/Capabilities/Capabilities';
 import Casses from './components/Casses/Casses';
 import Clients from './components/Clients/Clients';
 import FrontPage from './components/FrontPage/FrontPage';
+import Values from './components/Values/Values';
 import Welcome from './components/Welcome/Welcome';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Capabilities />
       <Clients />
       <Casses />
+      <Values />
     </React.Fragment>
   );
 }
---
+++ src/components/Values/Values.js
@@ -0,0 +1,13 @@
+import classes from './Values.module.css'
+
+const Values = () => {
+    return (
+        <div className={classes.values}>
+            <h1>CREATIVE  <span>IDEAS</span></h1>
+            <h1>PRACTICAL  <span>ACTION</span></h1>
+            <h1>LASTING  <span>CHANGE</span></h1>
+        </div>
+    );
+}
+
+export default Values;
